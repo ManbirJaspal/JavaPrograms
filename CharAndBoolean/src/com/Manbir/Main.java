6a04d71ef1a836a29746caf505e1ebbceb7b2fc0
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-04-29 18:30:37 -0400

    char and booleans

---
+++ CharAndBoolean/src/com/Manbir/Main.java
@@ -0,0 +1,15 @@
+package com.Manbir;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char myChar = 'D';
+
+        //Boolean can only consist of true and false
+        boolean myBoolean= false;
+        boolean isMale = true;
+
+    }
+
+}
